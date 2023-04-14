97c43e547e4d00f59c6f2cb46f9b847272ea0870
Aby <7082849+abynim@users.noreply.github.com>
2023-04-14 16:03:04 +0800

    Update tooltip.jsx

--- react-demo/src/components/tooltip/examples/tooltip.jsx
+++ react-demo/src/components/tooltip/examples/tooltip.jsx
@@ -3,7 +3,6 @@ import { VegaTooltip, VegaButton } from "@heartlandone/vega-react";
 const App = () => {
   return (
     <VegaTooltip
-      size={100}
       content={`This is a tooltip`}
       trigger={`hover`}
       placement={`top`}
